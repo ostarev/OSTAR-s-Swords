19fbb225acc734309dbfa7fc9c789bca1ee4b9a8
ostarev <ostaver31@gmail.com>
2026-02-17 12:50:45 +0100

    Rename AncientGlobal to RarityGlobal and update logic

--- Rarities/GlobalRarities.cs
+++ Rarities/GlobalRarities.cs
@@ -4,14 +4,21 @@ using OSTARsSWORDS.Rarities;
 
 namespace OSTARsSWORDS.Rarities
 {
-    public class AncientGlobal : GlobalItem
+    public class RarityGlobal : GlobalItem
     {
         public override bool PreDrawTooltipLine(Item item, DrawableTooltipLine line, ref int yOffset)
         {
             // Check if this is the item name line and if it has our custom rarity
+            //Ancient
             if (line.Mod == "Terraria" && line.Name == "ItemName" && item.rare == ModContent.RarityType<Ancient>())
             {
                 Ancient.Draw(item, line);
+                return false; // Skip the default drawing
+            }
+            //Inferno
+            if (line.Mod == "Terraria" && line.Name == "ItemName" && item.rare == ModContent.RarityType<Inferno>())
+            {
+                Inferno.Draw(item, line);
                 return false; // Skip the default drawing
             }
             return true;
