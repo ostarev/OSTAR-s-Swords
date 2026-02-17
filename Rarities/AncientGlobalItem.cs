380e48a0c0747d3a189d0587dec498d274c881d3
ostarev <ostaver31@gmail.com>
2026-02-17 12:44:56 +0100

    Update comment for clarity in PreDrawTooltipLine

--- Rarities/AncientGlobalItem.cs
+++ Rarities/AncientGlobalItem.cs
@@ -8,7 +8,7 @@ namespace OSTARsSWORDS.Rarities
     {
         public override bool PreDrawTooltipLine(Item item, DrawableTooltipLine line, ref int yOffset)
         {
-            // Check if this is the item name line and if it has our custom rarity
+            // Check the item name line and if it has our rarity
             if (line.Mod == "Terraria" && line.Name == "ItemName" && item.rare == ModContent.RarityType<Ancient>())
             {
                 Ancient.Draw(item, line);
